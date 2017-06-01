666877afb2da02a81871fc34707f5e7c99ab2bb7
Fighter-FT <tan@fantandeMac-mini.local>
2017-06-01 10:59:57 +0800

    取消默认的占位图
    取消默认的占位图取消默认的占位图

--- TableViewNoNata/NoDataTableView/NoDataView.m
+++ TableViewNoNata/NoDataTableView/NoDataView.m
@@ -16,7 +16,8 @@
 }
 
 - (void)createUI {
-    self.backgroundColor = [UIColor colorWithRed:231/255.0 green:231/255.0 blue:231/255.0 alpha:1];
+    //    self.backgroundColor = [UIColor colorWithRed:231/255.0 green:231/255.0 blue:231/255.0 alpha:1];
+    self.backgroundColor = [UIColor clearColor];
     [self addSubview:self.reloadButton];
 }
 
@@ -26,8 +27,8 @@
         _reloadButton.frame = CGRectMake(0, 0, 150, 150);
         _reloadButton.center = self.center;
         _reloadButton.layer.cornerRadius = 75.0;
-        [_reloadButton setBackgroundImage:[UIImage imageNamed:@"sure_placeholder_error"] forState:UIControlStateNormal];
-        [_reloadButton setTitle:@"暂无数据，点击重新加载!" forState:UIControlStateNormal];
+        [_reloadButton setBackgroundImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+        [_reloadButton setTitle:@"" forState:UIControlStateNormal];
         [_reloadButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
         [_reloadButton setTitleEdgeInsets:UIEdgeInsetsMake(200, -50, 0, -50)];
         [_reloadButton addTarget:self action:@selector(reloadClick:) forControlEvents:UIControlEventTouchUpInside];
